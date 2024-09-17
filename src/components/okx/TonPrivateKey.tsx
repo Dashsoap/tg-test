157dc90ae4367f844664eef1ac931ddb401694d5
eddie <eddie-fe@gate.me>
2024-09-17 22:25:25 +0800

    fix

---
+++ src/components/okx/TonPrivateKey.tsx
@@ -0,0 +1,146 @@
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import {
+    Alert,
+    AlertIcon,
+    Button,
+    Flex,
+    Heading,
+    IconButton,
+    Input,
+    InputGroup,
+    InputLeftAddon,
+    InputRightElement,
+    Select,
+    useClipboard,
+    useToast,
+    VStack,
+} from "@chakra-ui/react";
+import { CheckIcon, CopyIcon } from "@chakra-ui/icons";
+import { TonWallet } from "@okxweb3/coin-ton";
+import AddressOutput from "./AddressOutput";
+import Box100 from './Box100';
+import { errorToast, successToast } from "./toast";
+
+const BOUNCEABLE = 'bounceable';
+const UNBOUNCEABLE = 'unbounceable';
+
+interface GeneratePrivateKeyProps {
+    wallet: TonWallet;
+    privateKey: string;
+    setPrivateKey: React.Dispatch<React.SetStateAction<any>>;
+}
+
+const TonPrivateKey: React.FC<GeneratePrivateKeyProps> = (props) => {
+    const toast = useToast();
+
+    const [address, setAddress] = useState('');
+    const [network, setNetwork] = useState('mainnet');
+    const [addressBounceable, setAddressBounceable] = useState(UNBOUNCEABLE);
+
+    const { hasCopied: hasCopiedPrivKey, onCopy: onCopyPrivKey } = useClipboard(props.privateKey);
+
+    const updateAddress = async () => {
+        const privateKey = props.privateKey;
+        getAddress(privateKey)
+            .then((addr) => {
+                setAddress(addr);
+                successToast(toast, "地址更新成功");
+            })
+            .catch((e) => {
+                errorToast(toast, e.toString());
+            });
+    };
+
+    const getAddress = async (privateKey: string) => {
+        const { address: a } = await props.wallet.getNewAddress({ privateKey });
+        const { address: addr } = await props.wallet.parseAddress({ address: a });
+        return addr.toString({
+            urlSafe: true,
+            bounceable: addressBounceable === BOUNCEABLE,
+            testOnly: network === 'testnet',
+        });
+    };
+
+    const handleSelectAddressBounceable = (event: ChangeEvent<HTMLSelectElement>) => {
+        setAddressBounceable(event.target.value);
+    };
+
+    const handleSelectNetworkChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setNetwork(event.target.value);
+    };
+
+    const handleUpdateAddress = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const privateKey = props.privateKey;
+        const addr = await getAddress(privateKey);
+        setAddress(addr);
+    };
+
+    const handlePrivKeyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { value } = e.target;
+        props.setPrivateKey(value);
+    };
+
+    const handleGenPrivKey = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const privateKey = await props.wallet.getRandomPrivateKey();
+        props.setPrivateKey(privateKey);
+
+        const addr = await getAddress(privateKey);
+        setAddress(addr);
+    };
+
+    useEffect(() => {
+        updateAddress();
+    }, [addressBounceable, network]);
+
+    return (
+        <Box100>
+            <VStack spacing={4} align="stretch" w="100%">
+                <Heading size="md">生成私钥</Heading>
+
+                <Select value={network} onChange={handleSelectNetworkChange} size="sm">
+                    <option value="mainnet">主网</option>
+                    <option value="testnet">测试网3</option>
+                </Select>
+                <InputGroup size="sm">
+                    <InputLeftAddon>私钥</InputLeftAddon>
+                    <Input
+                        type="text"
+                        value={props.privateKey}
+                        onChange={handlePrivKeyChange}
+                        placeholder="输入私钥"
+                    />
+                    <InputRightElement>
+                        <IconButton
+                            onClick={onCopyPrivKey}
+                            size="sm"
+                            variant="ghost"
+                            icon={hasCopiedPrivKey ? <CheckIcon /> : <CopyIcon />}
+                            aria-label={hasCopiedPrivKey ? '已复制' : '复制'}
+                        />
+                    </InputRightElement>
+                </InputGroup>
+
+                <Select value={addressBounceable} onChange={handleSelectAddressBounceable} size="sm">
+                    <option value={BOUNCEABLE}>可跳转地址</option>
+                    <option value={UNBOUNCEABLE}>不可跳转地址</option>
+                </Select>
+
+                <Flex gap={2} direction="column">
+                    <Button onClick={handleGenPrivKey} size="sm" colorScheme="blue" width="100%">
+                        生成随机私钥
+                    </Button>
+                    <Button onClick={handleUpdateAddress} size="sm" colorScheme="blue" width="100%">
+                        更新地址
+                    </Button>
+                </Flex>
+
+                <AddressOutput address={address} />
+            </VStack>
+        </Box100>
+    );
+};
+
+export default TonPrivateKey;
---
+++ src/components/okx/TonSignJettonTx.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    Heading,
+    useToast,
+    VStack
+} from "@chakra-ui/react";
+import Box100 from "./Box100";
+import FormInput from "./FormInput";
+import { TransactionFormProps, TonJettonTxData } from "./types";
+import SignedOutput from "./SignedOutput";
+import FormCheckbox from "./FormCheckbox";
+import { errorToast, successToast } from "./toast";
+
+
+const TonSignJettonTx: React.FC<TransactionFormProps> = (props) => {
+    const toast = useToast()
+    const [txData, setTxData] = useState<TonJettonTxData>({
+        type: "jettonTransfer", // Jetton TOKEN 转账类型
+        fromJettonAccount: "kQDL9sseMzrh4vewfQgZKJzVwDFbDTpbs2f8BY6iCMgRTyOG", // 发起者的 Jetton 钱包地址
+        to: "UQDXgyxgYKNSdTiJBqmNNfbD7xuRMl6skrBmsEtyXslFm5an", // 接收者地址
+        seqno: 15, // 发起者地址的 nonce 或序列号
+        toIsInit: false, // 接收地址是否已初始化
+        memo: "jetton test", // 交易备注
+        decimal: 2, // TON 区块链上的 Jetton TOKEN 小数位
+        amount: "100", // TOKEN 的小数位为 2，因此这里的真实值为 1
+        messageAttachedTons: "50000000", // 消息费，默认为 0.05，这里为 0.05 * 10^9
+        invokeNotificationFee: "1", // 通知费，官方推荐 0.000000001，这里为 0.000000001 * 10^9
+        /**
+         * export enum SendMode {
+         *     CARRY_ALL_REMAINING_BALANCE = 128,
+         *     CARRY_ALL_REMAINING_INCOMING_VALUE = 64,
+         *     DESTROY_ACCOUNT_IF_ZERO = 32,
+         *     PAY_GAS_SEPARATELY = 1,
+         *     IGNORE_ERRORS = 2,
+         *     NONE = 0
+         * }
+         */
+        sendMode: 1,
+        queryId: "18446744073709551615" // uint64 数字的字符串，例如 18446744073709551615 = 2^64 - 1
+    });
+    const [tx, setTx] = useState('')
+
+    const [scroll, setScroll] = useState(false);
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        props.wallet.signTransaction({ privateKey: props.privateKey, data: txData })
+            .then(data => {
+                setTx(data.boc)
+                setScroll(true);
+                successToast(toast, "签名交易成功")
+            })
+            .catch(e => {
+                setTx('')
+                errorToast(toast, e)
+            })
+    };
+    return (
+        <Box100 scroll={scroll} setScroll={setScroll}>
+            <Heading marginBottom="5">签名 Jetton 交易</Heading>
+            <VStack spacing={2} align="stretch" w={"100%"}>
+                <FormInput name={"to"} data={txData} setData={setTxData} />
+                <FormInput name={"amount"} data={txData} setData={setTxData} />
+                <FormInput name={"seqno"} data={txData} setData={setTxData} />
+                <FormInput name={"decimal"} data={txData} setData={setTxData} />
+                <FormInput name={"memo"} data={txData} setData={setTxData} />
+                <FormCheckbox name={"toInit"} data={txData} setData={setTxData} />
+                <FormInput name={"messageAttachedTons"} data={txData} setData={setTxData} />
+                <FormInput name={"invokeNotificationFee"} data={txData} setData={setTxData} />
+                <FormInput name={"queryId"} data={txData} setData={setTxData} />
+                <FormInput name={"publicKey"} data={txData} setData={setTxData} />
+                <Button onClick={handleSubmit} type="submit" colorScheme="blue" width="full">
+                    签名交易
+                </Button>
+                <SignedOutput name="签名交易结果" output={tx} />
+            </VStack>
+        </Box100>
+    );
+};
+
+export default TonSignJettonTx;
